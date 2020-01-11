131f8e06f7fd76fbd6385ac9c34a6e4d9c4914e2
Michael Telford <michael.telford@live.com>
2020-01-11 13:50:00 +0000

    Added app with /login form

---
+++ config.ru
@@ -0,0 +1,22 @@
+require 'rack'
+require 'rack/cache'
+require_relative 'helpers'
+
+app = Rack::Builder.new do
+  use Rack::Cache,
+    metastore:   'file:/var/cache/rack/meta',
+    entitystore: 'file:/var/cache/rack/body',
+    verbose:     true
+
+  map '/login' do
+    handler = proc { |env| [200, {}, [body(login_form)]] }
+    run handler
+  end
+
+  map '/auth' do
+    handler = proc { |env| [200, {}, [body('TODO')]] }
+    run handler
+  end
+end
+
+run app
---
+++ helpers.rb
@@ -0,0 +1,31 @@
+
+def body(html)
+    <<~HTML
+    <html>
+        <head></head>
+        <body>#{html}</body>
+    </html>
+    HTML
+end
+
+def login_form
+    <<~HTML
+    <br />
+    <form method="post" action="/auth">
+        <div>
+            <label for="username">Username</label>
+            <br />
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <br />
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <br />
+            <input type="submit" value="Login">
+        </div>
+    </form>
+    HTML
+end
